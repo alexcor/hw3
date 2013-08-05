34ac174469d4699868ec97ac921a99ba5fb66c06
Alex Correia <acorreia@gmail.com>
2013-08-05 18:30:11 +0100

    c5

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -5,7 +5,7 @@ Given /the following movies exist/ do |movies_table|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
     # ActiveRecord.create(movie)
-    debbuger
+    debugger
   end
   # flunk "Unimplemented"
 end
