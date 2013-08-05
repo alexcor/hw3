65dbb91742c8cfdb4724c1224cf6fb245297cb2a
Alex Correia <acorreia@gmail.com>
2013-08-05 20:31:14 +0100

    c12

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -28,6 +28,9 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
   rating_list.split.each do |rating|
-  	debugger
+  	if uncheck then
+  		uncheck rating
+  	else
+  		check rating
   end
 end
