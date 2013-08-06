510e2260d44d9bfe65563c874b3efc7b264c510d
Alex Correia <acorreia@gmail.com>
2013-08-06 07:56:17 +0100

    c24

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -27,8 +27,8 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  rating_list.split.each do |rating|
-  	if uncheck then
+  rating_list.split(', ').each do |rating|
+  	if uncheck=="un" then
   		uncheck "ratings_"+rating
   	else
   		check "ratings_"+rating
