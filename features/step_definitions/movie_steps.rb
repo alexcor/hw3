5301f875f6c2baffbf6b5ecf6739e76b8d1eb894
Alex Correia <acorreia@gmail.com>
2013-08-06 12:03:02 +0100

    c22

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -8,6 +8,9 @@ Given /the following movies exist/ do |movies_table|
     # debugger
   end
   # flunk "Unimplemented"
+
+  assert Movie.all.count == movies_table.hashes.size
+
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -17,6 +20,18 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
   flunk "Unimplemented"
+end
+
+Then /^I should see none of the movies$/ do
+    moviesInPage = page.all("table#movies tbody tr td[1]").map! {|t| t.text}
+  	assert moviesInPage.size == 0
+	pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see all of the movies$/ do
+    moviesInPage = page.all("table#movies tbody tr td[1]").map! {|t| t.text}
+  	assert Movie.all.count == moviesInPage.size
+	pending # express the regexp above with the code you wish you had
 end
 
 # Make it easier to express checking or unchecking several boxes at once
